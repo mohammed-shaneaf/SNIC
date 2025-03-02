ac61943d22ad37c72f3f4cbf07e5b3952ad0ab1b
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-03-02 11:49:37 +0300

    added app_text_styles to the project

---
+++ lib/core/utils/widgets/app_text_styles.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle sfProTextLight24 = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w300,
+    fontSize: 24,
+  );
+
+  static const TextStyle robotoRegular16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const TextStyle sfProDisplayRegular25 = TextStyle(
+    fontFamily: 'SF Pro Display',
+    fontWeight: FontWeight.w400,
+    fontSize: 25,
+  );
+
+  static const TextStyle robotoBold16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+
+  static const TextStyle sfProTextBold10 = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.bold,
+    fontSize: 10,
+  );
+
+  static const TextStyle sfProTextSemibold15 = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+  );
+
+  static const TextStyle robotoRegular20 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+
+  static const TextStyle robotoSemiBold16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  static const TextStyle coconBold24 = TextStyle(
+    fontFamily: 'CoconÆ Next Arabic',
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+
+  static const TextStyle robotoMedium20 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+
+  static const TextStyle ibmPlexSansArabicBold24 = TextStyle(
+    fontFamily: 'IBM Plex Sans Arabic',
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+}
